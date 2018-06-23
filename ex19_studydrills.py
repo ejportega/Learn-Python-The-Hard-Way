ec893e9a86115f7c76cd9028a1ca660b00ab60e9
dummy9285 <dummy9285@gmail.com>
2018-06-23 20:04:23 +0800

    commit

---
+++ ex19_studydrills.py
@@ -0,0 +1,5 @@
+def add(num1, num2):
+    print("{} + {} answer is {}".format(num1, num2, num1 + num2))
+
+# num1 = int(input("Enter first number:"))
+add(int(input("Enter first number: ")), int(input("Enter second number: ")))
